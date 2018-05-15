bd65b347e81f311a53cd6019141ed9a72b2a6141
wadegb12 <wadegb@okstate.edu>
2018-05-14 23:12:59 -0500

    added two column layout and photos to src page

--- Pages/_SRCInfo.php
+++ Pages/_SRCInfo.php
@@ -2,46 +2,101 @@
 <link type="text/css" rel="stylesheet" href="/../CSS/Utilities.css">
 
 <div class="container grayBackground hide-on-med-and-down">
-  <div class="linePadding">
-    <div class="title"> State Referee Chairman </div>
-    <div> Richard Brook </div>
-    <div> Broken Arrow, OK </div>
-    <div> 918-688-9873 </div>
-    <div> Richard@oksoccer.com </div>
+  <div class="row linePadding">
+    <div class="col s6">
+      <div class="center">
+        <div class="title"> State Referee Chairman </div>
+        <div> Richard Brook </div>
+        <div> Broken Arrow, OK </div>
+        <div> 918-688-9873 </div>
+        <div> Richard@oksoccer.com </div>
+      </div>
+    </div>
+    <div class="col s6">
+      <div class="center">
+        <img class="srcPhoto" src="/../Photos/RichardBrook.JPG">
+      </div>
+    </div>
   </div>
-  <div class="linePadding">
-    <div class="title"> State Referee Administrator </div>
-    <div> Ronald Poulsen </div>
-    <div> Tulsa, OK </div>
-    <div> 918-521-6286 </div>
-    <div> ron@oksoccer.com </div>
+  <div class="row linePadding">
+    <div class="col s6">
+      <div class="center">
+        <div class="title"> State Referee Administrator </div>
+        <div> Ronald Poulsen </div>
+        <div> Tulsa, OK </div>
+        <div> 918-521-6286 </div>
+        <div> ron@oksoccer.com </div>
+      </div>
+    </div>
+    <div class="col s6">
+      <div class="center">
+        <img class="srcPhoto" src="/../Photos/RonPoulsen.jpg">
+      </div>
+    </div>
   </div>
-  <div class="linePadding">
-    <div class="title"> State Youth Referee Administrator </div>
-    <div> Nate Hopkins </div>
-    <div> Broken Arrow, OK </div>
-    <div> 918-289-1629 </div>
-    <div> nate@oksoccer.com </div>
+  <div class="row linePadding">
+    <div class="col s6">
+      <div class="center">
+        <div class="title"> State Youth Referee Administrator </div>
+        <div> Nate Hopkins </div>
+        <div> Broken Arrow, OK </div>
+        <div> 918-289-1629 </div>
+        <div> nate@oksoccer.com </div>
+      </div>
+    </div>
+    <div class="col s6">
+      <div class="center">
+        <img class="srcPhoto" src="/../Photos/NateHopkins.PNG">
+      </div>
+    </div>
   </div>
-  <div class="linePadding">
-    <div class="title"> State Director of Assessment </div>
-    <div> Larry Morgan </div>
-    <div> Jenks, OK </div>
-    <div> 918-740-2697 </div>
-    <div> bokconstr@aol.com </div>
+  <div class="row linePadding">
+    <div class="col s6">
+      <div class="center">
+        <div class="title"> State Director of Assessment </div>
+        <div> Larry Morgan </div>
+        <div> Jenks, OK </div>
+        <div> 918-740-2697 </div>
+        <div> bokconstr@aol.com </div>
+      </div>
+    </div>
+    <div class="col s6">
+      <div class="center">
+        <img class="srcPhoto" src="/../Photos/LarryMorgan.png">
+      </div>
+    </div>
   </div>
-  <div class="linePadding">
-    <div class="title"> State Director of Instruction </div>
-    <div> Jim Brittain </div>
-    <div> Broken Arrow, OK </div>
-    <div> 918-231-9744 </div>
-    <div> jimb@pobox.com </div>
+  <div class="row linePadding">
+    <div class="col s6">
+      <div class="center">
+        <div class="title"> State Director of Instruction </div>
+        <div> Jim Brittain </div>
+        <div> Broken Arrow, OK </div>
+        <div> 918-231-9744 </div>
+        <div> jimb@pobox.com </div>
+      </div>
+    </div>
+    <div class="col s6">
+      <div class="center">
+        <img class="srcPhoto" src="/../Photos/JimBrittain.JPG">
+      </div>
+    </div>
   </div>
-  <div class="linePadding">
-    <div class="title"> State Director of Assignment </div>
-    <div> Darron Killman </div>
-    <div> Oklahoma City, OK </div>
-    <div> 405-255-9731 </div>
-    <div> Darron.killman@sbcglobal.net </div>
+  <div class="row linePadding">
+    <div class="col s6">
+      <div class="center">
+        <div class="title"> State Director of Assignment </div>
+        <div> Darron Killman </div>
+        <div> Oklahoma City, OK </div>
+        <div> 405-255-9731 </div>
+        <div> Darron.killman@sbcglobal.net </div>
+      </div>
+    </div>
+    <div class="col s6">
+      <div class="center">
+        <img class="srcPhoto" src="/../Photos/DarronKillman.JPG">
+      </div>
+    </div>
   </div>
+
 </div>
