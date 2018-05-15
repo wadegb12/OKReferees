f0058b25c6158e4cbb19fa699c4a99c64cd76df5
wadegb12 <wadegb@okstate.edu>
2018-05-14 22:49:53 -0500

    nav bar items will on ever appear on one line

--- Pages/NavBar.php
+++ Pages/NavBar.php
@@ -2,22 +2,26 @@
   <div class="container">
 
     <ul id="rulesDropdown" class="dropdown-content">
-      <li><a href="ComingSoon.php">SRPL</a></li>
-      <li><a href="ComingSoon.php">OPL/OCL</a></li>
-      <li><a href="ComingSoon.php">OPC</a></li>
-      <li><a href="ComingSoon.php">ECNL</a></li>
-      <li><a href="ComingSoon.php">Rec\Small Sided</a></li>
+      <li><a href="ComingSoon.php"><div class="navBarText"> SRPL </div></a></li>
+      <li><a href="ComingSoon.php"><div class="navBarText"> OPL/OCL </div></a></li>
+      <li><a href="ComingSoon.php"><div class="navBarText"> OPC </div></a></li>
+      <li><a href="ComingSoon.php"><div class="navBarText"> ECNL </div></a></li>
+      <li><a href="ComingSoon.php"><div class="navBarText"> Rec\Small Sided </div><</a></li>
     </ul>
      <ul id="navBar">
-        <li><a href="Home.php">Home</a></li>
-        <li><a href="ComingSoon.php">Policies</a></li>
-        <li><a href="ComingSoon.php">Calander</a></li>
-        <li><a class="dropdown-button" data-coverTrigger="false" data-activates="rulesDropdown" data-hover="true" >Rules</a></li>
-        <li><a href="_SRCInfo.php">SRC Info</a></li>
-        <li><a href="ComingSoon.php">Upgrade</a></li>
-        <li><a href="ComingSoon.php">Referee Status</a></li>
-        <li><a href="ComingSoon.php">Evaluations</a></li>
-        <li><a href="_Links.php">Links</a></li>
+        <li><a href="Home.php"><div class="navBarText"> Home </div></a></li>
+        <li><a href="ComingSoon.php"><div class="navBarText"> Policies </div></a></li>
+        <li><a href="ComingSoon.php"><div class="navBarText"> Calander </div></a></li>
+        <li>
+          <a class="dropdown-button" data-coverTrigger="false" data-activates="rulesDropdown" data-hover="true">
+            <div class="navBarText"> Rules </div>
+          </a>
+        </li>
+        <li><a href="_SRCInfo.php"><div class="navBarText"> SRC Info </div></a></li>
+        <li><a href="ComingSoon.php"><div class="navBarText"> Upgrade </div></a></li>
+        <li><a href="ComingSoon.php"><div class="navBarText"> Referee Status </div></a></li>
+        <li><a href="ComingSoon.php"><div class="navBarText"> Evaluations </div></a></li>
+        <li><a href="_Links.php"><div class="navBarText"> Links </div></a></li>
      </ul>
 
   </div>
