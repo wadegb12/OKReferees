00d230e38f9f26d293711659e9b3ea3d39354280
wade.beall <wadegb@okstate.edu>
2018-05-27 17:03:48 -0500

    edited referee status table

--- SQL/CREATE_TABLE_referee_status.sql
+++ SQL/CREATE_TABLE_referee_status.sql
@@ -1,6 +1,6 @@
 CREATE TABLE referee_status (
     full_name varchar(50),
-    grade int,
+    grade varchar(2),
     game_count bool,
     upgrade_clinic bool,
     written_test bool,
@@ -11,7 +11,23 @@ CREATE TABLE referee_status (
 
 INSERT INTO referee_status (full_name, grade, game_count, upgrade_clinic, 
 	written_test, recertification_clinic, assessment, fitness_test) 
+VALUES ('Wade', 7, 0, 0, 0, 0, 0, 0);
+
+INSERT INTO referee_status (full_name, grade, game_count, upgrade_clinic, 
+	written_test, recertification_clinic, assessment, fitness_test) 
+VALUES ('Wade', 7A, 0, 0, 0, 0, 0, 0);
+
+INSERT INTO referee_status (full_name, grade, game_count, upgrade_clinic, 
+	written_test, recertification_clinic, assessment, fitness_test) 
 VALUES ('Wade', 6, 0, 0, 0, 0, 0, 0);
+
+INSERT INTO referee_status (full_name, grade, game_count, upgrade_clinic, 
+	written_test, recertification_clinic, assessment, fitness_test) 
+VALUES ('Wade', 5R, 0, 0, 0, 0, 0, 0);
+
+INSERT INTO referee_status (full_name, grade, game_count, upgrade_clinic, 
+	written_test, recertification_clinic, assessment, fitness_test) 
+VALUES ('Wade', 5A, 0, 0, 0, 0, 0, 0);
 
 
 select * from referee_status;
