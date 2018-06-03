eb34fd7cf316623e9cf25aa3f12af89126703e76
wade.beall <wadegb@okstate.edu>
2018-06-03 01:22:30 -0500

    edited layout of login page

--- App/login.php
+++ App/login.php
@@ -37,19 +37,14 @@
 <!DOCTYPE html>
 <html>
   <head>
-  <title>Login Form in PHP with Session</title>
-  <link href="style.css" rel="stylesheet" type="text/css">
-  </head>
-  <body>
-  <div id="main">
-  <h1>PHP Login Session Example</h1>
+  
   <div id="login">
   <h2>Login Form</h2>
   <form action="" method="post">
   <label>UserName :</label>
-  <input id="name" name="username" placeholder="username" type="text">
+  <input id="name" name="username" type="text">
   <label>Password :</label>
-  <input id="password" name="password" placeholder="**********" type="password">
+  <input id="password" name="password" type="password">
   <input name="submit" type="submit" value=" Login ">
   <span> <?php echo $error; ?>
   </span>
