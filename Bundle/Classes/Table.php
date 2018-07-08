0df852e0a465debf2ab0972d9e3bf6f9b0a26d85
wade.beall <wadegb@okstate.edu>
2018-07-08 17:48:34 -0500

    working status table

--- Bundle/Classes/Table.php
+++ Bundle/Classes/Table.php
@@ -1,51 +1,55 @@
 <?php
 
     class Table {
+        public $columnConfig = array();
         
         public function createDataTable($tableData) 
         {
             $html = "";
             $html .= "<table id=\"status_table\" class=\"display\" style=\"width:100%\">";
-            if($tableData === [])
-            {
-                $blankTableData = $this->getBlankTableData();
-                $html .= "<thead>" . $this->addColumns($blankTableData) . "</thead>";
-            }
-            else {
-                $html .= "<thead>" . $this->addColumns($tableData) . "</thead>";
-                $html .= "<tbody>" . $this->addRows($tableData) . "</tbody>";
-            } 
-            
+            $html .= "<thead>" . $this->addColumns($tableData) . "</thead>";
+            $html .= "<tbody>" . $this->addRows($tableData) . "</tbody>";
             $html .= "</table>";
             
             return $html;
         }
 
-        public function addColumns($result) 
+        public function addColumns() 
         {
             $html = "";
-            foreach($result as $row) {
-                $html .= "<tr>";
-                foreach($row as $columnName => $value) {
-                    $html .= "<th>" . $columnName . "</th>";
-                }
-                $html .= "</tr>";
-                return $html;
+            $html .= "<tr>";
+            foreach($this->columnConfig as $column) {
+                $html .= "<th>" . $column . "</th>";
             }
+            $html .= "</tr>";
             return $html;
         }
         
         public function addRows($result) 
         {
+            $value = "";
             $html = "";
             foreach($result as $row) {
                 $html .= "<tr>";
-                foreach($row as $columnName => $value) {
+                $rowKeys = array_keys($row);
+
+                foreach($this->columnConfig as $column) {
+                    if(in_array($column, $rowKeys)) {
+                        $value = $row[$column];
+                    }
+                    else {
+                        $value = "-";
+                    }
                     $html .= "<td>" . $value . "</td>";
                 }
+                
                 $html .= "</tr>";
             }
             
             return $html;
+        }
+
+        public function setColumnConfig($columnConfig) {
+            $this->columnConfig = $columnConfig;
         }
     }
--- Bundle/Controllers/RefereeStatusController.php
+++ Bundle/Controllers/RefereeStatusController.php
@@ -1,14 +1,14 @@
 <?php
     class RefereeStatusController extends AbstractController {
-        private $db;
         private $error = "";
         private $mysqlExcption = "";
-        private $statusData;
+        private $statusData = array();
         private $assessmentData;
         private $statusTableData = array();
         private $statusTable = "";
         private $interactiveQueries = "";
-        
+        private $blankTableColumns = array('Name', 'Grade', 'Recert Clinic', 'Written Test', 'Assessment', 
+            'Fitness Test', 'Game Log', 'Upgrade Cinic');
 
         public function index() {
             $this->db = new Database();
@@ -16,17 +16,20 @@
             $this->table = new Table();
             $this->exceptionHandler = new ExceptionHandler();
 
-
             if($this->exceptionHandler->isValidConnToDB($this->db->conn)) {
                 $this->statusData = $this->db->exeQuery($this->queries->getStatusesQuery());
                 $this->assessmentData = $this->db->exeQuery($this->queries->getAssessmentsQuery());
 
                 $refereeAssessments = $this->getRefereeAssessments();
-                $this->mergeStatusWithAssessments($refereeAssessments);
-                $this->statusTableData = $this->removeId($this->statusTableData);
                 
-                $this->statusTableData[1]['Assessment 2'] = "-";
-                $this->statusTableData[1]['Assessment 3'] = "-";
+                if (!is_null($this->statusData)) {
+                    if(count($refereeAssessments) > 0) {
+                        $this->statusData = $this->mergeStatusWithAssessments($this->statusData, $refereeAssessments);
+                    }
+
+                    $this->statusTableData = $this->removeId($this->statusData);
+                }
+                
                 $this->statusTable = $this->createStatusTable($this->statusTableData);
 
                 
@@ -39,6 +42,100 @@
             $this->buildView();
             $this->renderHTML($this->view);
         }
+
+        private function getRefereeAssessments() {
+            $assessments = array();
+
+            if (!is_null($this->assessmentData)) {
+                foreach($this->assessmentData as $row => $data) {
+                    $refereeIds = array_keys($assessments);
+
+                    if(in_array($data["referee_id"], $refereeIds)) {
+                        array_push($assessments[$data["referee_id"]], $data["Assessment 1"]);
+                    }
+                    else {
+                        $assessments[$data["referee_id"]] = array($data["Assessment 1"]);
+                    }
+                }
+            }
+
+            return $assessments;
+        }
+
+        private function mergeStatusWithAssessments($statusData, $refereeAssessments) {
+            $mergedArray = array();
+
+            foreach($statusData as $row => $data) {
+                array_push($mergedArray, $data); 
+                foreach($refereeAssessments as $refereeId => $assessments) {
+
+                    if($refereeId == $data["id"]) {
+                        $i = 1;
+                        foreach($assessments as $key => $value) {
+                            $mergedArray[$row]["Assessment " . $i] = $value;
+                            $i++;
+                        }
+                    }
+                }
+            }
+
+            return $mergedArray;
+        }
+
+        private function removeId($array) {
+            $newArray = array();
+
+            foreach($array as $row => $data) {
+                foreach($data as $key => $value) {
+                    if($key !== "id") {
+                        $newArray[$row][$key] = $value;
+                    }
+                }
+            }
+
+            return $newArray;
+        }
+
+        private function createStatusTable($tableData) {
+            $table = "";
+
+            if(is_null($this->statusData)) {
+                $this->table->setColumnConfig($this->blankTableColumns);
+                $table = $this->table->createDataTable([]);
+            }
+            else if($this->exceptionHandler->isMysqliException($this->statusData)) {
+                $this->mysqlExcption = $this->statusData;
+                $this->table->setColumnConfig($this->getBlankTableData());
+                $table = $this->table->createDataTable([]);
+            }
+            else {
+                $this->table->setColumnConfig($this->getColumns($tableData));
+                $table = $this->table->createDataTable($tableData);
+            }
+
+            return $table;
+        }
+
+        private function getColumns($tableData) {
+            $dataWithMostCols = array();
+            $columns = array();
+
+            foreach($tableData as $row => $data) {
+                if(count($data) > count($dataWithMostCols)) {
+                    $dataWithMostCols = $data;
+                }
+            }
+
+            foreach($dataWithMostCols as $column => $value) {
+                array_push($columns, $column);
+            }
+
+            return $columns;
+        }
+
+        // private function createQueries() {
+            
+        // }
 
         private function buildView() {
             ob_start(); 
@@ -56,89 +153,6 @@
 
             <?php $this->view = ob_get_clean(); 
         }
-
-        // private function createQueries() {
-            
-        // }
-
-        private function createStatusTable($tableData) {
-            $table = "";
-
-            if(is_null($this->statusData)) {
-                $table = $this->table->createDataTable([]);
-            }
-            else if($this->exceptionHandler->isMysqliException($this->statusData)) {
-                $this->mysqlExcption = $this->statusData;
-                $table = $this->table->createDataTable([]);
-            }
-            else {
-                $table = $this->table->createDataTable($tableData);
-            }
-
-            return $table;
-        }
-
-        private function getRefereeAssessments() {
-            $assessments = array();
-
-            foreach($this->assessmentData as $row => $data) {
-                $refereeIds = array_keys($assessments);
-
-                if(in_array($data["referee_id"], $refereeIds)) {
-                    array_push($assessments[$data["referee_id"]], $data["Assessment 1"]);
-                }
-                else {
-                    $assessments[$data["referee_id"]] = array($data["Assessment 1"]);
-                }
-            }
-
-            return $assessments;
-        }
-
-        private function mergeStatusWithAssessments($refereeAssessments) {
-            foreach($this->statusData as $row => $data) {
-                array_push($this->statusTableData, $data); 
-                foreach($refereeAssessments as $refereeId => $assessments) {
-
-                    if($refereeId == $data["id"]) {
-                        $i = 1;
-                        foreach($assessments as $key => $value) {
-                            $this->statusTableData[$row]["Assessment " . $i] = $value;
-                            $i++;
-                        }
-                    }
-                }
-            }
-        }
-
-        private function removeId($array) {
-            $newArray = array();
-
-            foreach($array as $row => $data) {
-                foreach($data as $key => $value) {
-                    if($key !== "id") {
-                        $newArray[$row][$key] = $value;
-                    }
-                }
-            }
-
-            return $newArray;
-        }
-
-        private function getBlankTableData()
-        {
-            $array[0]['Name'] = '';
-            $array[0]['Grade'] = '';
-            $array[0]['Recert Clinic'] = '';
-            $array[0]['Written Test'] = '';
-            $array[0]['Assessment'] = '';
-            $array[0]['Fitness Test'] = '';
-            $array[0]['Game Log'] = '';
-            $array[0]['Upgrade Cinic'] = '';
-            return $array;
-        }
-
-        
         
     }
 
