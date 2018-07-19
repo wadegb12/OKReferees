f9746bea6a288d4ea4fce6933b3a1203dfc5fc3d
wade.beall <wadegb@okstate.edu>
2018-07-18 20:54:49 -0500

    removed login route

--- Setup/Routes.php
+++ Setup/Routes.php
@@ -9,7 +9,7 @@
         new Route('/links', 'LinksController', 'index'),
         new Route('/srcContacts' ,'SRCContactsController', 'index'),
         new Route('/refereeStatus' ,'RefereeStatusController', 'index'),
-        new Route('/login' ,'LoginController', 'index')
+        // new Route('/login' ,'LoginController', 'index')
     );
 
     $routes = $routesList;
